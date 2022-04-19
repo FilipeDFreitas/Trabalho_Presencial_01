2199611a17f8534b393ac44166a9ec25ec2a6b9e
Filipe de Freitas <filipefreitas@ufpr.br>
2022-04-18 23:24:56 -0300

    Minor hotfix

--- app/src/main/java/com/example/trabalho_presencial_01/MainActivity.java
+++ app/src/main/java/com/example/trabalho_presencial_01/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
         Aritbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                startActivity(new Intent(MainActivity.this, MyOtherActivity.class));
+               startActivity(new Intent(MainActivity.this, Arit.class));
             }
         });
     }
